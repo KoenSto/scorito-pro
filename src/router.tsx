4294de3ccd1ee06a962452572e442cb6960cf4d8
KoenSto <kst@pasbv.nl>
2026-07-01 21:12:44 +0200

    Add router configuration with main layout and routes

---
+++ src/router.tsx
@@ -0,0 +1,22 @@
+import { createHashRouter } from 'react-router-dom'
+import MainLayout from './layout/MainLayout'
+import Dashboard from './pages/Dashboard'
+import Team from './pages/Team'
+import Riders from './pages/Riders'
+import Stages from './pages/Stages'
+import Optimizer from './pages/Optimizer'
+import Settings from './pages/Settings'
+
+export const router = createHashRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      { path: '/', element: <Dashboard /> },
+      { path: '/team', element: <Team /> },
+      { path: '/riders', element: <Riders /> },
+      { path: '/stages', element: <Stages /> },
+      { path: '/optimizer', element: <Optimizer /> },
+      { path: '/settings', element: <Settings /> },
+    ],
+  },
+])
